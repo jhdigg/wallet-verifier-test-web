73819b05f80d26fc19abb9521bdd793f1d2f41e9
Eric Thelin <extern.eric.thelin@digg.se>
2025-07-03 14:18:39 +0200

    fix: quote command line arguments
    This avoids issues by whitespace in input data.
    
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

--- docker/config/eudi-verifier/import-cert.sh
+++ docker/config/eudi-verifier/import-cert.sh
@@ -18,16 +18,16 @@ fi
 echo "Importing certificate from $CERT_FILE..."
 
 # First, backup the original cacerts
-cp $JAVA_HOME/lib/security/cacerts $JAVA_HOME/lib/security/cacerts.original
+cp "$JAVA_HOME/lib/security/cacerts" "$JAVA_HOME/lib/security/cacerts.original"
 
 # Create a new truststore by copying the default Java truststore
-cp $JAVA_HOME/lib/security/cacerts $TRUSTSTORE
+cp "$JAVA_HOME/lib/security/cacerts" $TRUSTSTORE
 
 # Check if the file contains multiple certificates (certificate chain)
 cert_count=$(grep -c "BEGIN CERTIFICATE" "$CERT_FILE")
 echo "Found $cert_count certificate(s) in the file"
 
-if [ $cert_count -gt 1 ]; then
+if [ "$cert_count" -gt 1 ]; then
   # Split the certificate chain into individual files
   awk '/-----BEGIN CERTIFICATE-----/{f=sprintf("/tmp/cert_%02d.crt",++i)} f{print > f} /-----END CERTIFICATE-----/{close(f)}' "$CERT_FILE"
 
@@ -37,16 +37,16 @@ if [ $cert_count -gt 1 ]; then
     echo "Importing $cert_name..."
     keytool -import -trustcacerts -keystore $TRUSTSTORE -storepass $STOREPASS -noprompt -alias "$cert_name" -file "$i"
     # Also import to default cacerts for gradle wrapper
-    keytool -import -trustcacerts -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -noprompt -alias "$cert_name" -file "$i"
+    keytool -import -trustcacerts -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit -noprompt -alias "$cert_name" -file "$i"
   done
 
   # Clean up
   rm -f /tmp/cert_*.crt
 else
   # Single certificate
-  keytool -import -trustcacerts -keystore $TRUSTSTORE -storepass $STOREPASS -noprompt -alias proxy-cert -file $CERT_FILE
+  keytool -import -trustcacerts -keystore $TRUSTSTORE -storepass $STOREPASS -noprompt -alias proxy-cert -file "$CERT_FILE"
   # Also import to default cacerts for gradle wrapper
-  keytool -import -trustcacerts -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -noprompt -alias proxy-cert -file $CERT_FILE
+  keytool -import -trustcacerts -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit -noprompt -alias proxy-cert -file "$CERT_FILE"
 fi
 
 echo "Certificate(s) imported successfully to both truststores"
